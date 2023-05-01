13098b9fa95e34bc4e049c00869a3db2df234f16
L3tum <9307432+L3tum@users.noreply.github.com>
2023-05-01 14:27:47 +0200

    fix: Few issues that came from converting into separate repos

--- src/ServerSocket.php
+++ src/ServerSocket.php
@@ -5,7 +5,7 @@ namespace Uring;
 class ServerSocket extends Socket
 {
     public function __construct(
-        public readonly int           $socketFileDescriptor,
+        int           $socketFileDescriptor,
         public readonly SocketAddress $socketAddress
     )
     {
